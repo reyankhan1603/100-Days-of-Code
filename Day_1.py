678790f8483d561c325658596aab5ab7e3695b8f
Reyan Khan <reyankhan1603@gmail.com>
2024-01-08 23:49:23 +0530

    This is my first day of python code

---
+++ Day_1.py
@@ -0,0 +1,7 @@
+#Band Name Generator
+print("Welcome to  the Band Name Generator.")
+print("What's the name of the city you grew up in?")
+city = input()
+print("What'syour pet's name?")
+pet = input()
+print("Your bane name could be", city+" "+pet)
