c1aa2bd38491d0b5e51ca6ed036273ec7de795aa
Reyan Khan <reyankhan1603@gmail.com>
2024-01-24 03:53:23 +0530

    Maze Problem

---
+++ Day_6.py
@@ -0,0 +1,7 @@
+#The following code contains the maze problem link.
+
+#You'll have to visit a website  to solve this problem. I will not be providing solutions
+#but a link will be provided for the user to attempt the problems by himself/ herself.
+
+print("The link is: ")
+print("https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json")
