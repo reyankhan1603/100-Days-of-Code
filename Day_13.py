835923ab111d0c3b9f76a2361d72f464a0b1c79a
Reyan Khan <reyankhan1603@gmail.com>
2024-01-27 09:07:06 +0530

    Debugging notes

---
+++ Day_13.py
@@ -0,0 +1,16 @@
+#Day 13 is all about debugging
+#The following are the notes i took on debugging.
+
+"""
+The following are some helpful tips to debugg a program you have:
+1. Describe the problem.
+2. Reproduce the bugg to narrow down the cause of the bugg.
+3. Pretend to be the computer and evaluate each line.
+4. Fix the errors.
+5. Use print() statement to detect the bugg.
+6. Use a debugger.
+7. Take a break.
+8. Ask a friend.
+9. Run your code frequently during development to catch early buggs.
+10. Refer to stackoverflow.
+"""
