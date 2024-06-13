090363496557e4e233ecb79a0a45bfe0a024369b
Reyan Khan <reyankhan1603@gmail.com>
2024-06-13 19:11:22 +0530

    Turtle crossing game

---
+++ Day_23/main.py
@@ -0,0 +1,35 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.tracer(0)
+
+player = Player()
+car_manager = CarManager()
+
+screen.listen()
+
+game_is_on = True
+while game_is_on:
+
+    screen.onkey(player.move, "Up")
+    time.sleep(0.1)
+    screen.update()
+
+    car_manager.create_car()
+    car_manager.move_cars()
+
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+
+    if player.ycor() > 270:
+        player.finishline()
+        game_is_on = False
+
+
+screen.exitonclick()
