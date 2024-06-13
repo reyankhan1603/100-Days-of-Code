f64788e13ba189996f27cb943aee65d0e4e263af
Reyan Khan <reyankhan1603@gmail.com>
2024-06-13 19:13:19 +0530

    Turtle Crossing Game

--- Day_23/main.py
+++ Day_23/main.py
@@ -2,7 +2,6 @@ import time
 from turtle import Screen
 from player import Player
 from car_manager import CarManager
-from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=800, height=600)
